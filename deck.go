15c24662726e6fedee3e8fab05fae8acd8c84703
Nimira43 <n.rabone72@aol.com>
2024-11-14 21:49:47 +0000

    Create dunction Deck with card suits and values

--- deck.go
+++ deck.go
@@ -4,6 +4,12 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
