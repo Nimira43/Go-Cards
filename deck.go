43586b588d4f11efdd46877cec7b68bc11bfe881
Nimira43 <n.rabone72@aol.com>
2024-12-22 21:45:00 +0000

    Using for loops two create a full deck of cards

--- deck.go
+++ deck.go
@@ -6,8 +6,16 @@ type deck []string
 
 func newDeck() deck {
 	cards := deck{}
+
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+	return cards
 }
 
 func (d deck) print() {
@@ -15,3 +23,8 @@ func (d deck) print() {
 		fmt.Println(i, card)
 	}
 }
+
+// func main() {
+// 	cards := newDeck()
+// 	cards.print()
+// }
