7a63f207628223b4ec1155d991faa31b9265a99e
Nimira43 <n.rabone72@aol.com>
2024-12-22 21:48:05 +0000

    Updated code in main.go to run the newDeck() function - created full deck of cards

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 func main() {
-	cards := deck{"Seven of Hearts", newCard()}
-	cards = append(cards, "Four of Spaces")
+	cards := newDeck()
 
 	cards.print()
 }
 
-func newCard() string {
-	return "Nine of Clubs"
-}
