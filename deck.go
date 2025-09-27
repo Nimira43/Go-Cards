6bcba8f4a72566a32eb5757c67608a34e7a6a3e6
Nimira43 <n.rabone72@aol.com>
2025-09-27 21:46:39 +0100

    cardSuits

--- deck.go
+++ deck.go
@@ -4,7 +4,10 @@ import "fmt"
 
 type deck []string
 
-func newDeck() {}
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+}
 
 func (d deck) print() {
 	for i, card := range d {
