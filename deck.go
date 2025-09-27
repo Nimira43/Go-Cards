3324a76ea8356a16b996917dd4a95e33866cd17c
Nimira43 <n.rabone72@aol.com>
2025-09-27 21:54:41 +0100

    Creating cards by looping through suits and values and adding to cards

--- deck.go
+++ deck.go
@@ -5,13 +5,16 @@ import "fmt"
 type deck []string
 
 func newDeck() deck {
+	
 	cards := deck{}
+
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	
 	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
 	for i, suit := range cardSuits {
-		for j, value := range cardValue {
-			
+		for j, value := range cardValues {
+			cards = append(cards, value + " of " + suit)
 		}
 	}
 }
