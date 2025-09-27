bf607e81de6ab4a958fcaaa8fbb092c6dab60504
Nimira43 <n.rabone72@aol.com>
2025-09-27 21:49:11 +0100

    cardValues

--- deck.go
+++ deck.go
@@ -7,6 +7,7 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 }
 
 func (d deck) print() {
